68661176767bfd7a104071e1ce52346835382b5a
Dan-YY <dan@dansstudio.ca>
2021-04-25 16:57:31 -0700

    Initial Commit

---
+++ FrameWorkS/FrameWorkS.h
@@ -0,0 +1,18 @@
+//
+//  FrameWorkS.h
+//  FrameWorkS
+//
+//  Created by macbook on 2021-04-25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameWorkS.
+FOUNDATION_EXPORT double FrameWorkSVersionNumber;
+
+//! Project version string for FrameWorkS.
+FOUNDATION_EXPORT const unsigned char FrameWorkSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWorkS/PublicHeader.h>
+
+
